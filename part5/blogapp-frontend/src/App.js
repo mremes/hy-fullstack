bb82e29a61084fcaca9d7c7f5eac740d95a8fbe0
Matti Remes <matti.remes@gmail.com>
2019-02-24 22:46:10 +0200

    5.3-5.4 * Add functionality to add blog entries * Add notifications for user actions' results

--- part5/blogapp-frontend/src/App.js
+++ part5/blogapp-frontend/src/App.js
@@ -19,10 +19,14 @@ const Notification = ({ message, handler, className }) => {
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState("")
-  const [errorMessage, setErrorMessage] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -31,7 +35,9 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    setUser(JSON.parse(window.localStorage.getItem('user')))
+    const cachedUser = JSON.parse(window.localStorage.getItem('user'))
+    setUser(cachedUser)
+    blogService.setToken(cachedUser.token)
   }, [])
 
   const handleLogin = async (event) => {
@@ -43,13 +49,11 @@ const App = () => {
 
       setUser(user)
       window.localStorage.setItem('user', JSON.stringify(user))
+
       setUsername('')
       setPassword('')
     } catch (exception) {
       setErrorMessage('käyttäjätunnus tai salasana virheellinen')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
     }
   }
 
@@ -59,11 +63,33 @@ const App = () => {
     setUser(null)
   }
 
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = await blogService.createBlog({ title: newBlogTitle, author: newBlogAuthor, url: newBlogUrl })
+      setBlogs(blogs.concat(newBlog))
+      setSuccessMessage('blogi luotiin onnistuneesti')
+    } catch (error) {
+      setErrorMessage(`virhe blogin luonnissa: ${error.message}`)
+    }
+  }
+
   if (user) {
     return (
       <div>
         <p>Logged in as {user.name}</p>
         <button onClick={handleLogout}>logout</button>
+        <Notification message={errorMessage} handler={setErrorMessage} className="error" />
+        <Notification message={successMessage} handler={setSuccessMessage} className="success" />
+        <h2>create new</h2>
+        <div>
+          <form onSubmit={handleNewBlog}>
+            <div>title: <input type="text" name="newBlogTitle" value={newBlogTitle} onChange={({ target }) => setNewBlogTitle(target.value)} /></div>
+            <div>author: <input type="text" name="newBlogAuthor" value={newBlogAuthor} onChange={({ target }) => setNewBlogAuthor(target.value)} /></div>
+            <div>url: <input type="text" name="newBlogUrl" value={newBlogUrl} onChange={({ target }) => setNewBlogUrl(target.value)} /></div>
+            <button type="submit">create</button>
+          </form>
+        </div>
         <h2>blogs</h2>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
@@ -75,6 +101,7 @@ const App = () => {
     <div>
       <h2>log in to application</h2>
       <Notification message={errorMessage} handler={setErrorMessage} className="error" />
+      <Notification message={successMessage} handler={setSuccessMessage} className="success" />
       <form onSubmit={handleLogin}>
         <div>
           käyttäjätunnus
--- part5/blogapp-frontend/src/services/blogs.js
+++ part5/blogapp-frontend/src/services/blogs.js
@@ -2,9 +2,20 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const createBlog = (entry) => {
+  const request = axios.post(baseUrl, entry, { headers: { Authorization: token } })
+  return request.then(response => response.data)
+}
+
+export default { getAll, createBlog, setToken }
