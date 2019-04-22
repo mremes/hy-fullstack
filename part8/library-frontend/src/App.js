12d1bcb37351e0fc9bb6572b160d6599c073dee1
Matti Remes <remes@iki.fi>
2019-04-22 18:55:03 +0300

    8.1 - 8.7

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import { useQuery } from 'react-apollo-hooks'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql`
+{
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
+
+const ALL_BOOKS = gql`
+{
+  allBooks {
+    title
+    author
+    published
+  }
+}
+`
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const authors = useQuery(ALL_AUTHORS)
+  const books = useQuery(ALL_BOOKS)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+      <Authors show={page === 'authors'} result={authors} />
+      <Books show={page === 'books'} result={books} />
+      <NewBook show={page === 'add'} />
+
+    </div>
+  )
+}
+
+export default App
---
+++ part8/library-frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+            <App />
+        </ApolloHooksProvider>
+    </ApolloProvider>,
+    document.getElementById('root')
+)
